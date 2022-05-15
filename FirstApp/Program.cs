a827a29f51702071d5e9d31fc84eddb3ee79dc15
NazmeevRT <nazmeevrt@mail.ru>
2022-05-15 21:34:53 +0300

    Создание небольшой анкеты

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,8 +6,15 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string MyName = "Ruslan";
-            Console.WriteLine(MyName);
+            Console.Write ("Здравствуйте, ведите Ваше имя : ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст : ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату рождения : ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Вас зовут {0} \nВаш возраст {1}\nДата вашего рождения: {2}", name, age, birthdate);
+
+            Console.ReadKey();
         }
     }
 }
